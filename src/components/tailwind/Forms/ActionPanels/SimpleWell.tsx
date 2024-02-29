b1d744b1b4788c702141abd41974d88d943503bd
David Norton <pie_masters@hotmail.co.uk>
2024-02-29 12:44:07 +0000

    add action panels

---
+++ src/components/tailwind/Forms/ActionPanels/SimpleWell.tsx
@@ -0,0 +1,25 @@
+export default function Example() {
+  return (
+    <div className="bg-gray-50 sm:rounded-lg">
+      <div className="px-4 py-5 sm:p-6">
+        <h3 className="text-base font-semibold leading-6 text-gray-900">
+          Need more bandwidth?
+        </h3>
+        <div className="max-w-xl mt-2 text-sm text-gray-500">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatibus praesentium tenetur pariatur.
+          </p>
+        </div>
+        <div className="mt-5">
+          <button
+            type="button"
+            className="inline-flex items-center px-3 py-2 text-sm font-semibold text-gray-900 bg-white rounded-md shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+          >
+            Contact sales
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/tailwind/Forms/ActionPanels/Toggle.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Component from "./Toggle";
+
+const meta = {
+  title: "Forms/Action Panels",
+  component: Component,
+  decorators: [
+    (Story) => (
+      <div className="bg-gray-100">
+        <div className="py-12 mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div className="max-w-3xl mx-auto">
+            <Story />
+          </div>
+        </div>
+      </div>
+    ),
+  ],
+} satisfies Meta<typeof Component>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const WithToggle: Story = {};
