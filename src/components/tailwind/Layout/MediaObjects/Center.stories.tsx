b5f3d23288a7351c3786d6e4f1eb6ecc148813a1
David Norton <pie_masters@hotmail.co.uk>
2024-03-20 16:02:13 +0000

    added media objects

---
+++ src/components/tailwind/Layout/MediaObjects/Center.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Component from "./Center";
+
+const meta = {
+  title: "Layout/Media Objects",
+  component: Component,
+  decorators: [
+    (Story) => (
+      <div className="p-8 bg-white">
+        <div className="max-w-md mx-auto">
+          <Story />
+        </div>
+      </div>
+    ),
+  ],
+} satisfies Meta<typeof Component>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const AlignedToCenter: Story = {};
